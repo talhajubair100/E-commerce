60da17cfd28117f8f61c921baed4281bd759643a
Talha Jubair <talhajubair100.bd@gmail.com>
2020-12-04 18:19:53 +0600

    slug field auto added

--- product/models.py
+++ product/models.py
@@ -17,7 +17,7 @@ class Category(MPTTModel):
     description = models.TextField()
     image = models.ImageField(blank=True ,upload_to='category_image')
     status = models.CharField(max_length=5, choices=STATUS)
-    slug = models.SlugField()
+    slug = models.SlugField(null=False, unique=True)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
@@ -28,6 +28,9 @@ class Category(MPTTModel):
     class MPTTMeta:
         order_insertion_by = ['title']
 
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
     def __str__(self):                           # __str__ method elaborated later in
         full_path = [self.title]                  # post.  use __unicode__ in place of
         k = self.parent
@@ -35,7 +38,7 @@ class Category(MPTTModel):
             full_path.append(k.title)
             k = k.parent
         return ' / '.join(full_path[::-1])
-        
+
         
 
 class Product(models.Model):
@@ -52,7 +55,7 @@ class Product(models.Model):
     amount = models.IntegerField()
     minamount=models.IntegerField()
     detail = RichTextUploadingField()
-    slug = models.SlugField()
+    slug = models.SlugField(null=False, unique=True)
     status = models.CharField(max_length=5,choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
@@ -64,6 +67,9 @@ class Product(models.Model):
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = 'Image'
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
 
 
 class Images(models.Model):
