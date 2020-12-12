034eb96ac9a9b487f1eb1327eb08b795baddefbd
Talha Jubair <talhajubair100.bd@gmail.com>
2020-12-13 01:13:02 +0600

    shopcart function implemnt

--- order/forms.py
+++ order/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from .models import ShopCart
 
-class ShopCart(forms.ModelForm):
+class ShopCartForm(forms.ModelForm):
     class Meta:
         model = ShopCart
         fields = ['quantity']
--- order/views.py
+++ order/views.py
@@ -1,7 +1,9 @@
-from django.http.response import HttpResponse
+from django.contrib import messages
+from .models import ShopCart
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .forms import ShopCartForm
 
 # Create your views here.
 def order(request):
@@ -9,7 +11,42 @@ def order(request):
 
 @login_required
 def add_to_shopcart(request, id):
-    # url = request.META.get("HTTP_REFERER")
-    # current_user = request.user
+    url = request.META.get('HTTP_REFERER')
+    current_user = request.user
 
-    pass
+    checkproduct = ShopCart.objects.filter(product_id=id)
+    
+    if checkproduct:
+        control = 1
+    else:
+        control = 0 
+
+    if request.method == 'POST':
+        form = ShopCartForm(request.POST)
+        if form.is_valid():
+            if control==1:
+                data = ShopCart.objects.get(product_id=id)
+                data.quantity += form.cleaned_data['quantity']
+                data.save()
+            else:
+                data = ShopCart()
+                data.user_id = current_user.id
+                data.product_id = id
+                data.quantity = form.changed_data['quantity']
+                data.save()
+        messages.success(request, "Product added to Shopcart")
+        return HttpResponseRedirect(url)
+
+    else:
+        if control==1:
+            data = ShopCart.objects.get(product_id=id)
+            data.quantity += 1
+            data.save()
+        else:
+            data = ShopCart()
+            data.user_id = current_user.id
+            data.product_id = id
+            data.quantity = 1
+            data.save()
+        messages.success(request, "Product added to Shopcart")
+        return HttpResponseRedirect(url)
