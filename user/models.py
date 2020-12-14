2101d369f08d7763e66c12e88ed6816aebdd9fa0
Talha Jubair <talhajubair100.bd@gmail.com>
2020-12-15 00:58:18 +0600

    user model added

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(blank=True, max_length=15)
+    address = models.CharField(blank=True, max_length=150)
+    city = models.CharField(blank=True, max_length=50)
+    country = models.CharField(blank=True, max_length=50)
+    image = models.ImageField(blank=True, upload_to='images/users/')
+
+    def __str__(self):
+        return self.user.username
