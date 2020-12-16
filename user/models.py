7c853c8b20f8169aa15af441aa5a02c0df4af452
Talha Jubair <talhajubair100.bd@gmail.com>
2020-12-16 19:01:10 +0600

    user admin added

--- user/models.py
+++ user/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.utils.safestring import mark_safe
+
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,7 +10,14 @@ class UserProfile(models.Model):
     address = models.CharField(blank=True, max_length=150)
     city = models.CharField(blank=True, max_length=50)
     country = models.CharField(blank=True, max_length=50)
-    image = models.ImageField(blank=True, upload_to='images/users/')
+    image = models.ImageField(blank=True, upload_to='media/users/')
 
     def __str__(self):
         return self.user.username
+
+    def user_name(self):
+        return self.user.first_name + ' ' + self.user.last_name + '[' + self.user.username + ']'
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = "User Image"
