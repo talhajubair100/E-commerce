8759d02ee6b65a0ff9d1a7af3d3f65ea9ed31d28
Talha Jubair <talhajubair100.bd@gmail.com>
2020-12-17 00:43:58 +0600

    user login function create

--- user/views.py
+++ user/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
 from product.models import Category
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from.models import UserProfile
 
 # Create your views here.
 def user_profile(request):
@@ -9,6 +12,20 @@ def user_profile(request):
 
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            
+
+
+            return HttpResponseRedirect("/")
+        else:
+            messages.warning(request, "Login Error !! Username or Password is incorrect")
+            return HttpResponseRedirect("/login")
+
     category = Category.objects.all()
     context = {'category': category}
     return render (request, 'login.html', context)
