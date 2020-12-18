4c875c3292df31e0bb562375898aeb76c39bb6c7
Talha Jubair <talhajubair100.bd@gmail.com>
2020-12-18 20:35:01 +0600

    admin file update

--- order/admin.py
+++ order/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ShopCart
+from .models import ShopCart, Order, OrderProduct
 # Register your models here.
 
 
@@ -8,6 +8,24 @@ class ShopCartAdmin(admin.ModelAdmin):
     list_display = ['product', 'user', 'quantity', 'price', 'amount']
     list_filter = ['user']
 
+class OrderProductline(admin.TabularInline):
+    model = OrderProduct
+    readonly_fields = ('user', 'product', 'price', 'quantity', 'amount')
+    can_delete = False
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'phone', 'city', 'total', 'status']
+    list_filter = ['status']
+    readonly_fields = ('user', 'first_name', 'last_name', 'address', 'city', 'country', 'phone', 'ip', 'total')
+    can_delete = False
+    inlines = [OrderProductline]
+
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'price','quantity','amount']
+    list_filter = ['user']
 
 
 admin.site.register(ShopCart, ShopCartAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderProduct, OrderProductAdmin)
