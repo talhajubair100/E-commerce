c861f882817f01147f509b8e766eca70d0289835
Talha Jubair <talhajubair100.bd@gmail.com>
2020-12-18 19:34:50 +0600

    order product model created

--- order/forms.py
+++ order/forms.py
@@ -11,5 +11,4 @@ class ShopCartForm(forms.ModelForm):
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['firdt_name', 'last_name', 'address', 'phone', 'city', 'country']
-        
+        fields = ['first_name', 'last_name', 'address', 'phone', 'city', 'country']
