5c2f925ce8512ae8055e5a9d3a98af731128043f
Talha Jubair <talhajubair100.bd@gmail.com>
2020-12-18 18:55:21 +0600

    order model created

--- order/models.py
+++ order/models.py
@@ -19,3 +19,30 @@ class ShopCart(models.Model):
 
     def __str__(self):
         return self.product.title
+
+class Order(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('Accepted', 'Accepted'),
+        ('Preaparing', 'Preaparing'),
+        ('OnShipping', 'OnShipping'),
+        ('Completed', 'Completed'),
+        ('Canceled', 'Canceled'),
+    )
+    user = models.ForeignKey(User , on_delete=models.SET_NULL, null=True)
+    code = models.CharField(max_length=5, editable=False)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=14, blank=True)
+    address = models.CharField(max_length=150, blank=True)
+    city = models.CharField(blank=True, max_length=50)
+    country = models.CharField(blank=True, max_length=50)
+    total = models.FloatField()
+    status = models.CharField(max_length=10, choices=STATUS, default='New')
+    ip = models.CharField(blank=True, max_length=50)
+    admin_note = models.CharField(blank=True, max_length=150)
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.first_name
