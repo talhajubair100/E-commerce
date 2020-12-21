eda4710f2583bfc22ef1ec3fef829596c3c0163c
Talha Jubair <talhajubair100.bd@gmail.com>
2020-12-21 23:48:16 +0600

    profile_update form added

--- user/forms.py
+++ user/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
+from django.forms import fields
+from django.forms import widgets
 from django.forms.widgets import TextInput
+from .models import UserProfile
 #from django.forms import widgets
 
 class SignUpForm(UserCreationForm):
@@ -23,4 +26,18 @@ class UserUpdateForm(UserChangeForm):
             'email': forms.EmailInput(attrs={'class': 'input','placeholder':'E-Mail'}),
             'first_name': forms.TextInput(attrs={'class': 'input','placeholder':'First Name'}),
             'last_name': forms.TextInput(attrs={'class': 'input','placeholder':'Last Name'}),
+        }
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('phone', 'address', 'city', 'country', 'image')
+        widgets = {
+            'phone': forms.TextInput(attrs={'class': 'input','placeholder':'Phone'}),
+            'address': forms.TextInput(attrs={'class': 'input','placeholder':'Address'}),
+            'city': forms.TextInput(attrs={'class': 'input','placeholder':'City'}),
+            'country': forms.TextInput(attrs={'class': 'input','placeholder':'Country'}),
+            'image': forms.FileInput(attrs={'class': 'input','placeholder':'Image'}),
+
+
         }
