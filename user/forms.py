db8224879975ee498908314ea2e9533b344d44be
Talha Jubair <talhajubair100.bd@gmail.com>
2020-12-21 23:30:22 +0600

    user_update forms added

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
+from django.forms.widgets import TextInput
+#from django.forms import widgets
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(max_length=50, label="User Name")
@@ -11,3 +13,14 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', )
+
+class UserUpdateForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'input','placeholder':'Username'}),
+            'email': forms.EmailInput(attrs={'class': 'input','placeholder':'E-Mail'}),
+            'first_name': forms.TextInput(attrs={'class': 'input','placeholder':'First Name'}),
+            'last_name': forms.TextInput(attrs={'class': 'input','placeholder':'Last Name'}),
+        }
